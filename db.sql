ed505ef838d8fa7f0974c3c36458fd6e2f98fe3d
huss <loic.huss@gmail.com>
2021-03-23 01:18:45 +0100

    [MODIF] action as post to be more rest compliant

--- db.sql
+++ db.sql
@@ -3,9 +3,13 @@ CREATE TABLE `items` (
   `id`   INT UNSIGNED AUTO_INCREMENT NOT NULL,
   `type` ENUM('stream','file','playlist') NOT NULL,
   `name` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `path` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `path` VARCHAR(512) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL UNIQUE,
+  `description` TEXT CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `country` CHAR(2) DEFAULT NULL,
   
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `test`.`items` (`id`, `type`, `name`, `path`) VALUES (NULL, 'stream', 'nowy swiat', 'https://n04a-eu.rcs.revma.com/ypqt40u0x1zuv')
+INSERT INTO `test`.`items` (`id`, `type`, `name`, `path`, `description`, `country`) VALUES (NULL, 'stream', 'Nowy Świat', 'https://de1.api.radio-browser.info/pls/url/429ed05b-6dae-4027-a9be-7fda436a89fe', 'AAC', 'pl');
+INSERT INTO `test`.`items` (`id`, `type`, `name`, `path`, `description`, `country`) VALUES (NULL, 'stream', 'Nowy Świat', 'https://n04a-eu.rcs.revma.com/ypqt40u0x1zuv', NULL, 'pl');
+INSERT INTO `test`.`items` (`id`, `type`, `name`, `path`, `description`, `country`) VALUES (NULL, 'stream', 'France Info', 'https://de1.api.radio-browser.info/pls/url/aa555e8a-08d9-11e8-ae97-52543be04c81', 'fr');
--- server.js
+++ server.js
@@ -50,7 +50,31 @@ function player_action_play(path) {
   const url = build_player_base_url();
   url.searchParams.append('command', 'in_play');
   url.searchParams.append('input', path);
-  
+  return url.href;
+}
+
+function player_action_stop() {
+  const url = build_player_base_url();
+  url.searchParams.append('command', 'pl_stop');
+  return url.href;
+}
+
+function player_action_pause() {
+  const url = build_player_base_url();
+  url.searchParams.append('command', 'pl_pause');
+  return url.href;
+}
+
+function player_action_set_volume(val) {
+  const url = build_player_base_url();
+  url.searchParams.append('command', 'volume');
+  url.searchParams.append('val', val);
+  return url.href;
+}
+
+function player_action_clear_pl() {
+  const url = build_player_base_url();
+  url.searchParams.append('command', 'pl_empty');
   return url.href;
 }
 
@@ -58,14 +82,63 @@ function player_action_status() {
   return build_player_base_url().href;
 }
 
-function player_parse_xml(data) {
+function _player_parse_xml(data) {
   const options = {
     attributeNamePrefix : "@_",
     ignoreAttributes : false,
     ignoreNameSpace : false,
   };
   return parser.parse(data, options);
-  //console.log(jsonObj.root.information.category[0].info);
+}
+
+function player_parse_xml(xml_string) {
+  // parse xml from player
+  const data_raw = _player_parse_xml(xml_string);
+
+  // build data
+  const data = {
+    currentplid: data_raw.root.currentplid,
+    time: data_raw.root.time,
+    volume: data_raw.root.volume,
+    length: data_raw.root.length,
+    random: data_raw.root.random,
+    state: data_raw.root.state,
+    loop: data_raw.root.loop,
+    position: data_raw.root.position,
+    repeat: data_raw.root.repeat,
+    stats: data_raw.root.stats
+  };
+
+  // get meta data
+  const top_meta_data = _.get(data_raw, ['root', 'information', 'category']);
+  if (_.isArray(top_meta_data) === true) {
+    const stream = {info: {}, meta: {}};
+    let attr = null;
+
+    for (let row of top_meta_data) {
+      switch (row['@_name']) {
+      case 'meta':
+        for (attr of row.info) {
+          if (_.has(attr, '@_name') && _.has(attr, '#text'))
+            stream.info[attr['@_name']] = attr['#text'];
+        }
+        break;
+
+      case 'Stream 0':
+        for (attr of row.info) {
+          if (_.has(attr, '@_name') && _.has(attr, '#text'))
+            stream.meta[attr['@_name']] = attr['#text'];
+        }
+        break;
+
+      default:
+        break;
+      }
+    }
+    // add meta data
+    data.stream = stream;
+  }
+  return data; 
 }
 
 // create a player process
@@ -95,11 +168,102 @@ v1.use(function(req, res, next) {
   next();
 });
 
+
+//---- STATUS
+v1.get('/', (req, res, next) => {
+  axios.get(player_action_status(), {
+    auth: {
+      username: (process.env.VLC_USER || ''),
+      password: process.env.VLC_PASSWORD,
+    }
+  }).then(player_res => {
+    res.status(200).send(player_parse_xml(player_res.data));
+    next();
+  }).catch(player_error => {
+    console.log(player_error);
+    res.sendStatus(400);
+    // handle error
+    next(player_error);
+  });
+});
+
+
+//---- VOLUME
+v1.get('/volume', (req, res, next) => {
+  axios.get(player_action_status(), {
+    auth: {
+      username: (process.env.VLC_USER || ''),
+      password: process.env.VLC_PASSWORD,
+    }
+  }).then(player_res => {
+    res.status(200).send({volume: player_parse_xml(player_res.data).volume});
+    next();
+  }).catch(player_error => {
+    // handle error
+    res.sendStatus(400);
+    // console.log(error);
+    next(player_error);
+  });
+});
+
+
+//---- COMMAND
+v1.post('/command', (req, res, next) => {
+  const { action } = req.body;
+
+  let func = null;
+  let args = [];
+
+  switch (action) {
+  case 'stop':
+    func = player_action_stop;
+    break;
+
+  case 'clear':
+    func = player_action_clear_pl;
+    break;
+
+  case 'pause':
+    func = player_action_pause;
+    break;
+
+  case 'volume':
+    func = player_action_set_volume;
+    let { val } = req.body;
+    args = [val];
+    break;
+
+  default:
+    break;
+  }
+
+  if (func === null) {
+    res.sendStatus(400);
+    next();
+  } else {
+    axios.get(func(...args), {
+      auth: {
+        username: (process.env.VLC_USER || ''),
+        password: process.env.VLC_PASSWORD,
+      }
+    }).then(player_res => {
+      res.status(200).send(player_parse_xml(player_res.data));
+      next();
+    }).catch(player_error => {
+      // handle error
+      res.sendStatus(400);
+      // console.log(error);
+      next(player_error);
+    });
+  }
+});
+
+//---- ITEMS
 v1.get('/items', (req, res, next) => {
   // const { file } = req.body;
 
   db_query(
-    'SELECT id, type, name, path, genre FROM items',
+    'SELECT id, type, name, path, description, country FROM items',
     (error, results, fields) => {
       if (error) throw error;
       res.status(200).send(results);
@@ -109,50 +273,30 @@ v1.get('/items', (req, res, next) => {
 });
 
 v1.get('/items/:key', (req, res, next) => {
-  if (req.params.key === 'status') {
-
-    axios.get(player_action_status(), {
-      auth: {
-        username: (process.env.VLC_USER || ''),
-        password: process.env.VLC_PASSWORD,
-      }
-    }).then(player_res => {
-      console.log(player_parse_xml(player_res.data));
-      res.status(200).send({});
+  db_query(
+    `SELECT id, type, name, path, description, country FROM items WHERE id=${req.params.key} LIMIT 1`,
+    (error, results, fields) => {
+      if (error) throw error;
+      res.status(200).send(results);
       next();
-    }).catch(player_error => {
-      console.log(player_error);
-      res.status(400);
-      // handle error
-      next(player_error);
-    });
-
-  } else {
-    db_query(
-      `SELECT id, type, name, path, genre FROM items WHERE id=${req.params.key} LIMIT 1`,
-      (error, results, fields) => {
-        if (error) throw error;
-        res.status(200).send(results);
-        next();
-      }
-    );
-  }
+    }
+  );
 });
 
 v1.post('/items', (req, res, next) => {
-  const { name, path, type } = req.body;
+  const { name, path, type, description, country } = req.body;
 
   if (_.isNil(name) || _.isNil(path) || _.isNil(type)) {
-    res.status(400);
+    res.sendStatus(400);
     next();
   } else {
     db_query(
-      `INSERT INTO test.items (id, type, name, path) VALUES (NULL, '${type}', '${name}', '${path}')`,
+      `INSERT INTO test.items (id, type, name, path, description, country) VALUES (NULL, '${type}', '${name}', '${path}', '${description}', '${country}')`,
       (error, results, fields) => {
         if (error) throw error;
 
         db_query(
-          `SELECT id, type, name, path FROM items WHERE id=${results.insertId} LIMIT 1`,
+          `SELECT id, type, name, path, description, country FROM items WHERE id=${results.insertId} LIMIT 1`,
           (err, result, f) => {
             if (err) throw error;
             res.status(200).send(_.head(result) || {});
@@ -165,7 +309,7 @@ v1.post('/items', (req, res, next) => {
 });
 
 v1.patch('/items/:key', (req, res, next) => {
-  const { name, path, type, play } = req.body;
+  const { name, path, type, description, country, play } = req.body;
 
   if (play === true) {
     // the user ask to play a specific item
@@ -177,7 +321,7 @@ v1.patch('/items/:key', (req, res, next) => {
         const path = (_.head(result) || {}).path;
 
         if (_.isNil(path)) {
-          res.status(400);
+          res.sendStatus(400);
           next();
         } else {
           axios.get(player_action_play(path), {
@@ -186,12 +330,11 @@ v1.patch('/items/:key', (req, res, next) => {
               password: process.env.VLC_PASSWORD,
             }
           }).then(player_res => {
-            //console.log(res);
             res.status(200).send(_.head(result) || {});
             next();
           }).catch(player_error => {
             // handle error
-            res.status(400);
+            res.sendStatus(400);
             // console.log(error);
             next(player_error);
           });
@@ -202,12 +345,12 @@ v1.patch('/items/:key', (req, res, next) => {
   } else {
 
     if (_.isNil(name) || _.isNil(path) || _.isNil(type)) {
-      res.status(400);
+      res.sendStatus(400);
       next();
     } else {
 
       db_query(
-        `UPDATE test.items SET type='${type}', name='${name}', path='${path}' WHERE id=${req.params.key} LIMIT 1`,
+        `UPDATE test.items SET type='${type}', name='${name}', path='${path}', description='${description}', country='${country}' WHERE id=${req.params.key} LIMIT 1`,
         (error, results, fields) => {
           if (error) throw error;
 
@@ -225,6 +368,17 @@ v1.patch('/items/:key', (req, res, next) => {
   }
 });
 
+v1.delete('/items/:key', (req, res, next) => {
+  db_query(
+    `DELETE FROM test.items WHERE id=${req.params.key} LIMIT 1`,
+    (error, results, fields) => {
+      if (error) throw error;
+
+      res.sendStatus(200);
+      next();
+    }
+  );  
+});
 
 //------------------------------------------------------------------------------ App
 const app = express();
@@ -241,15 +395,9 @@ http.createServer(app).listen(PORT, () => {
   console.log(`Audio Streaming System Running on http://127.0.0.1:${PORT}`);
 });
 
-
 // setTimeout(() => {
 //   player.stdin.write('add https://n04a-eu.rcs.revma.com/ypqt40u0x1zuv\n');
 // }, 3000);
-
-
-// setTimeout(() => {
-//   player.stdin.write('stop\n');
-// }, 5000);
 
 // curl -XGET 'http://127.0.0.1:8000/ass-service/v1/items'
 // curl -XPOST 'http://127.0.0.1:8000/ass-service/v1/items' -H "Content-Type: application/json" -d '{"name": "fip", "path": "bla", "type": "stream"}'
